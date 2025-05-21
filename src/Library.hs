ebe3a74a25bde92336f4e3fc8e88926368cf353f
Jose-Rey25 <jrey.frba.utn.edu.ar>
2025-05-21 19:41:11 -0300

    Guardado

--- src/Library.hs
+++ src/Library.hs
@@ -4,5 +4,49 @@ import PdePreludat
 doble :: Number -> Number
 doble numero = numero + numero
 
-triple :: Number -> Number
-triple numero = numero * 3
+data Campeon = UnCampeon {
+    antiguedad :: Number,
+    nivel :: Number,
+    posicion :: Posicion,
+    habilidades :: [String]
+} deriving (Show,Eq)
+
+data Posicion = TOP | ADC | SUPP | JG deriving (Show,Eq)
+
+chogath :: Campeon
+chogath = UnCampeon 15 8 TOP ["Ruptura", "Grito salvaje", "Clavos vorpalinos", "Festín"]
+
+jinx :: Campeon
+jinx = UnCampeon 12 4 ADC ["¡Cambio de armas!", "¡Chispas!", "¡Mascafuegos!", "¡¡Supermegacohete requetemortal!!"]
+
+mel :: Campeon
+mel = UnCampeon 0 6 SUPP ["Ráfaga radiante", "Refutar", "Ofuscador solar", "Eclipse dorado"]
+
+yuumi :: Campeon
+yuumi = UnCampeon 6 12 SUPP ["Proyectil acechador", "¡Tú y yo!", "Zoomies", "Últimas páginas"]
+
+warwick :: Campeon
+warwick = UnCampeon 16 1 JG ["Fauces de la bestia", "Cacería de sangre", "Aullido primigenio", "Opresión infinita"]
+
+equipo :: [Campeon]
+equipo = [chogath,jinx,mel,yuumi,warwick]
+
+----
+
+estaRoto :: Campeon -> Bool
+estaRoto campeon = antiguedad campeon == 0
+estaRoto2 :: Campeon -> Bool
+estaRoto2 = (==0).antiguedad
+
+subirDeNivel :: Campeon -> Campeon
+subirDeNivel campeon = campeon {nivel = nivel campeon + 1} 
+
+estaPreparado :: [Campeon] -> Bool
+estaPreparado = (==5).length
+
+estaEnEarly :: Campeon -> [String]
+estaEnEarly campeon | nivel campeon  < 6= take 3 (habilidades campeon)
+
+
+
+
